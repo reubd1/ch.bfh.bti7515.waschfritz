226380967a0983c0eb5f170e9123417eb95ef823
barta3 <baertschi.adrian@gmail.com>
2014-12-02 17:01:25 +0100

    Machine Service Impl + Test

---
+++ ch.bfh.bti7515.waschfritz.service/src/test/java/MachineServiceIT.java
@@ -0,0 +1,46 @@
+import ch.bfh.bti7515.waschfritz.service.MachineService;
+import ch.bfh.bti7515.waschfritz.service.dto.MachineDTO;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.inject.Inject;
+
+import static junit.framework.Assert.assertNull;
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by adrian on 02.12.14.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/serviceContext.xml")
+public class MachineServiceIT {
+
+    @Inject
+    private MachineService machineService;
+
+    @Test
+    public void test() {
+        MachineDTO machineDTO = new MachineDTO();
+        machineDTO.setName("Mach1");
+        machineDTO = machineService.create(machineDTO);
+
+        MachineDTO readMachine = machineService.read(machineDTO.getId());
+        assertEquals("Mach1", readMachine.getName());
+
+        readMachine.setName("New Name");
+        readMachine = machineService.update(readMachine);
+        MachineDTO updatedMachine = machineService.read(readMachine.getId());
+        assertEquals("New Name", readMachine.getName());
+
+        machineService.delete(updatedMachine);
+        MachineDTO deletedMachnine = machineService.read(readMachine.getId());
+        assertNull(deletedMachnine);
+
+
+    }
+
+
+}

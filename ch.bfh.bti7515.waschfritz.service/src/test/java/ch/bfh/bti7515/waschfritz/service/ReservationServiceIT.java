f3e27c21c6094870d5e63744b4be6bfeea217dd3
barta3 <baertschi.adrian@gmail.com>
2014-12-02 17:43:40 +0100

    Package fix

---
+++ ch.bfh.bti7515.waschfritz.service/src/test/java/ch/bfh/bti7515/waschfritz/service/ReservationServiceIT.java
@@ -0,0 +1,51 @@
+package ch.bfh.bti7515.waschfritz.service;
+
+import ch.bfh.bti7515.waschfritz.service.dto.ReservationDTO;
+import junit.framework.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.inject.Inject;
+import java.util.Date;
+
+/**
+ * Created by reubid on 02.12.14.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/serviceContext.xml")
+public class ReservationServiceIT {
+
+
+    @Inject
+    private ReservationService reservationService;
+
+    @Test
+    public void test() {
+
+        //create
+        ReservationDTO newReservation = new ReservationDTO();
+        newReservation.setEndDate(new Date());
+        newReservation = reservationService.create(newReservation);
+
+        //read
+        ReservationDTO readReservation = reservationService.read(newReservation.getId());
+        Assert.assertTrue(newReservation.getEndDate().equals(readReservation.getEndDate()));
+
+        //update
+        readReservation.setEndDate(new Date());
+        readReservation = reservationService.update(readReservation);
+        ReservationDTO updatedReservation = reservationService.read(readReservation.getId());
+        Assert.assertTrue(readReservation.getEndDate().equals(updatedReservation.getEndDate()));
+
+        //delete
+        reservationService.delete(updatedReservation);
+        ReservationDTO deleteReservation = reservationService.read(readReservation.getId());
+        Assert.assertNull(deleteReservation);
+
+    }
+
+
+}

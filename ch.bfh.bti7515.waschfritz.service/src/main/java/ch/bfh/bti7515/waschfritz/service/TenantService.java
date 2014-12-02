0efe0cf47416e55e0db12c6252fdc79b93b9ec53
barta3 <baertschi.adrian@gmail.com>
2014-12-02 17:24:08 +0100

    Tenant Service Impl + Test

--- ch.bfh.bti7515.waschfritz.service/src/main/java/ch/bfh/bti7515/waschfritz/service/TenantService.java
+++ ch.bfh.bti7515.waschfritz.service/src/main/java/ch/bfh/bti7515/waschfritz/service/TenantService.java
@@ -1,7 +1,21 @@
 package ch.bfh.bti7515.waschfritz.service;
 
+import ch.bfh.bti7515.waschfritz.service.dto.TenantDTO;
+
+import java.util.Collection;
+
 /**
  * Created by adrian on 02.12.14.
  */
 public interface TenantService {
+
+    public TenantDTO create(TenantDTO TenantDTO);
+
+    public TenantDTO read(long id);
+
+    public Collection<TenantDTO> list();
+
+    public TenantDTO update(TenantDTO machine);
+
+    public void delete(TenantDTO machine);
 }
